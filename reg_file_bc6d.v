5cc1e36ce3bd4aab7e8c04be2fe1ae20f3de93e3
zaileshar <zaileshar@gmail.com>
2022-12-28 15:17:46 +0530

    merge branch 'main'

---
+++ reg_file_bc6d.v
@@ -0,0 +1,10 @@
+module reg_file (
+    input clk,
+    input rst_n,
+    output reg out
+);
+    always @(posedge clk) begin
+        if (!rst_n) out <= 0;
+        else out <= ~out;
+    end
+endmodule
